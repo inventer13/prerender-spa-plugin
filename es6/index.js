d23f43ab5d3ad3c5f78f26111ef9d00c16823713
inventer13 <inventer13@163.com>
2018-12-07 10:45:28 +0800

    Update index.js
    <>&转义符转换

--- es6/index.js
+++ es6/index.js
@@ -119,11 +119,9 @@ PrerenderSPAPlugin.prototype.apply = function (compiler) {
           return mkdirp(path.dirname(processedRoute.outputPath))
             .then(() => {
               return new Promise((resolve, reject) => {
-                processedRoute.html = processedRoute.html.replace(/<title>(.*)<\/title>/, (params) => {
-                  params = params.replace('&lt;', '<')
-                  params = params.replace('&gt;', '>')
-                  return params
-                })
+                processedRoute.html = processedRoute.html.replace(/&lt;/g, '<')
+                processedRoute.html = processedRoute.html.replace(/&gt;/g, '>')
+                processedRoute.html = processedRoute.html.replace(/&amp;/g, '&')
                 compilerFS.writeFile(processedRoute.outputPath, processedRoute.html.trim(), err => {
                   if (err) reject(`[prerender-spa-plugin] Unable to write rendered route to file "${processedRoute.outputPath}" \n ${err}.`)
                   else resolve()
